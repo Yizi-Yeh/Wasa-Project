2803341ad69b7a971d8e463810ba37f9cf98b776
Yizi-Yeh <g384357@gmail.com>
2021-04-12 12:04:34 +0800

    Album.vue container-style

--- src/views/Album.vue
+++ src/views/Album.vue
@@ -3,49 +3,62 @@
     id="section-album"
     class="section-album"
   >
-    <div class="input-group">
-      <select v-model="currCategory">
-        <option selected>
-          ALL
-        </option>
-        <option
-          v-for="(category,idx) in categoryList"
-          :key="idx"
+    <div class="container album-container">
+      <div class="input-group">
+        <select
+          v-model="currCategory"
+          class="album-select"
         >
-          {{ category }}
-        </option>
-      </select>
-      <ul class="Album-lists">
-        <li
-          v-for="item in currAlbum"
-          :key="item"
-          class="Album-info wraps"
-        >
-          <div class="album-info">
-            <i class="fas fa-landmark" />
-            <img
-              :src="item.images"
-              width="250px"
-              alt=""
-            >
-          </div>
+          <option selected>
+            ALL
+          </option>
+          <option
+            v-for="(category,idx) in categoryList"
+            :key="idx"
+          >
+            {{ category }}
+          </option>
+        </select>
 
-          <div class="album-info">
-            <i class="fas fa-landmark" />
-            <span>ALBUM TITLE: {{ item.title }}</span>
-          </div>
-
-          <div class="album-info">
-            <i class="fas fa-map-marked-alt" />
-            <span>ALBUM DESCRIPTION: {{ item.description }}</span>
-          </div>
-
-          <div class="album-info">
-            <i class="far fa-calendar-alt" />
-            公告日期: {{ item.release }}
-          </div>
-        </li>
-      </ul>
+        <ul class="album-lists">
+          <li
+            v-for="item in currAlbum"
+            :key="item"
+            class="album-lists-info"
+          >
+            <div class="album-lists-info-img">
+              <i class="fas fa-landmark" />
+              <img
+                :src="item.images"
+                width="250px"
+                alt=""
+              >
+            </div>
+            <br>
+            <div class="album-lists-info-content">
+              <div class="album-lists-info-title">
+                <h5> {{ item.title }}</h5>
+              </div>
+              <br>
+              <div class="album-lists-info-intro">
+                <i class="fas fa-compact-disc" />
+                <span> 紹介： <br> {{ item.description }}</span>
+              </div>
+              <br>
+              <div class="album-lists-info-release">
+                <i class="far fa-calendar-alt" />
+                リリース：
+                <br> {{ item.release }}
+              </div>
+              <br>
+              <div class="album-lists-info-price">
+                <i class="fas fa-dollar-sign" />
+                值段：<br> {{ item.price }}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
